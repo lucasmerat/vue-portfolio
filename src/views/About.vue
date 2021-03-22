5819e43962119a8eaa7813310c91878c8eb15674
lucas merat <lucas.merat@gmail.com>
2021-03-22 15:51:34 -0500

    initial vue setup with routes and tailwind

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    this is my About
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    this is my home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
